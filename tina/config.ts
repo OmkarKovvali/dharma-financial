98c54f7a27617c395ea5f75f15f2e88a7bf8544d
OmkarKovvali <okovvali5@gmail.com>
2025-08-23 18:24:00 -0400

    Add Tina CMS Setup for testing

---
+++ tina/config.ts
@@ -0,0 +1,80 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "tina-cms-test"; // Changed to test branch
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!, // Using environment variable
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN!,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+      // Adding a pages collection for your main site content
+      {
+        label: "Pages",
+        name: "page",
+        path: "content/pages",
+        fields: [
+          {
+            type: "string",
+            label: "Title",
+            name: "title",
+          },
+          {
+            type: "string",
+            label: "Hero Text",
+            name: "hero",
+          },
+          {
+            type: "rich-text",
+            label: "Content",
+            name: "content",
+          },
+        ],
+      },
+    ],
+  },
+});
